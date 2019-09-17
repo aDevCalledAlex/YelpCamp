1256a4ba61b0e6a0a1efc4f1af78c378db8763fb
aDevCalledAlex <daphrat@gmail.com>
2019-09-17 13:43:44 -0400

    Added price and websiteURL attributes to Campground

--- models/campground.js
+++ models/campground.js
@@ -5,6 +5,8 @@ const mongoose         = require('mongoose'),
                             name : String,
                             image : String,
                             description : String,
+                            price : String,
+                            websiteURL : String,
                             author : {
                               id : {
                                 type : mongoose.Schema.Types.ObjectId,
