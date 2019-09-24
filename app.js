fa9b6a8886424872e6b09ba7a5e3f1a1ac22e286
aDevCalledAlex <daphrat@gmail.com>
2019-09-24 13:42:44 -0400

    added persistent sessions

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const dotenv              = require('dotenv').config(),
       port                = process.env.PORT || 3000,
       bodyParser          = require('body-parser'),
       expressSanitizer    = require('express-sanitizer'),
+      session             = require('express-session'),
+      MongoStore          = require('connect-mongo')(session),
       rp                  = require('request-promise'),
       methodOverride      = require('method-override'),
       flash               = require('connect-flash'),
@@ -29,18 +31,19 @@ const dotenv              = require('dotenv').config(),
 console.log(mongoURI);
 
 mongoose.connect(mongoURI, mongoConnectOptions)
-          .catch( err => {
-            console.log(err);
-          });
+          .catch( err => { console.log(err); });
 
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 app.use(express.static(`${__dirname}/public`)); // for including this folder into the scope (stylesheet/script directory)
 app.use(bodyParser.urlencoded({ extended : true })); // just copypasta, needed to enable bodyParser (populate req.body)
-app.use(require('express-session')({
+app.use(session({
   secret : "I'm not sure if this is a good secret.",
   resave : false, // resave and saveUnitialized is copypasta
-  saveUninitialized : false
+  saveUninitialized : true,
+  store : new MongoStore({
+    mongooseConnection : mongoose.connection
+  })
 }));
 app.use(flash());
 app.use(passport.initialize());
